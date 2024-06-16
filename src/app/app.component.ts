cde6da22cdd45e2f2a24f49d49336b8c6a629bf3
ANIMESH PANDEY <animeshpandeyit@gmail.com>
2024-06-17 02:08:40 +0530

    localeEs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Component, LOCALE_ID, Inject, OnInit } from '@angular/core';
+import '@angular/localize/init';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'Multi-Language';
+export class AppComponent implements OnInit {
+  // title = 'Multi-Language';
+
+  title = 'i18nDemo';
+
+  languageList = [
+    { code: 'hi', label: 'हिंदी' },
+    { code: 'es', label: 'Espanol' },
+  ];
+  constructor(@Inject(LOCALE_ID) protected localeId: string) {}
+
+  ngOnInit(): void {
+    // Register the Spanish locale data
+    registerLocaleData(localeEs, 'es');
+
+    // Bootstrap the Angular AppModule
+    //   platformBrowserDynamic()
+    //     .bootstrapModule(AppModule)
+    //     .catch((err) => console.error(err));
+    // }
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
+/// <reference types="@angular/localize" />
+import { Component, OnInit } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+registerLocaleData(localeEs, 'es');
+
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
+
+// platformBrowserDynamic().bootstrapModule(AppModule)
+//   .catch(err => console.error(err));
